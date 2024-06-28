4cacb3fd8b8212dca8b896acb1f598cfdb4aff08
Mdzikri <dzikriftf@gmail.com>
2024-06-28 10:27:21 +0700

    Initial commit without credentials

---
+++ dags/data_transfer_dag.py
@@ -0,0 +1,37 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.mysql_hook import MySqlHook
+from airflow.utils.dates import days_ago
+import pandas as pd
+from sqlalchemy import create_engine
+
+def load_data_to_mysql():
+    # Gunakan MySqlHook untuk mendapatkan koneksi dari UI Airflow
+    hook = MySqlHook(mysql_conn_id='mysql_conn')
+    engine = create_engine(hook.get_uri())
+    # Baca dataset
+    df = pd.read_csv('/opt/airflow/dags/data/online_gaming_behavior_dataset.csv')
+    # Masukkan data ke tabel MySQL
+    df.to_sql('cek', con=engine, if_exists='replace', index=False)
+
+dag = DAG(
+    'data_transfer_to_mysql',
+    default_args={
+        'owner': 'airflow',
+        'depends_on_past': False,
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+    },
+    description='A simple DAG to transfer data to MySQL',
+    schedule_interval='@once',
+    start_date=days_ago(1),
+)
+
+transfer_data = PythonOperator(
+    task_id='load_data_to_mysql',
+    python_callable=load_data_to_mysql,
+    dag=dag,
+)
+
+transfer_data
---
+++ dags/to_gcs.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.hooks.mysql_hook import MySqlHook
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.utils.dates import days_ago
+import pandas as pd
+import os
+
+def export_data_from_mysql():
+    hook = MySqlHook(mysql_conn_id='mysql_conn')
+    engine = hook.get_sqlalchemy_engine()
+    df = pd.read_sql('SELECT * FROM cek', con=engine)
+    local_path = '/opt/airflow/dags/data/exported_data.csv'
+    df.to_csv(local_path, index=False)
+    return local_path
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+}
+
+with DAG(
+    'data_transfer_to_gcs',
+    default_args=default_args,
+    description='DAG untuk mengekspor data dari MySQL dan mengunggah ke GCS',
+    schedule_interval='@once',
+    start_date=days_ago(1),
+) as dag:
+
+    export_data_task = PythonOperator(
+        task_id='export_data_from_mysql',
+        python_callable=export_data_from_mysql,
+    )
+
+    upload_to_gcs_task = LocalFilesystemToGCSOperator(
+        task_id='upload_to_gcs',
+        src="{{ task_instance.xcom_pull(task_ids='export_data_from_mysql') }}",
+        dst='data/exported_data.csv',
+        bucket='testing-de',
+        gcp_conn_id='google_cloud_default',  # Gunakan nama koneksi GCS yang benar
+    )
+
+    export_data_task >> upload_to_gcs_task
